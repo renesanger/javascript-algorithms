a36c73c098d28ca811ad79b1677ef486e93e5eac
renesanger <renejsanger@gmail.com>
2026-03-18 16:22:02 -0400

    Implement isAnagram method to check anagrams

---
+++ is-anagram.js
@@ -0,0 +1,21 @@
+class Solution {
+  isAnagram(t,s) {
+    if(t.length != s.length)
+      return false;
+
+    const countS = {}
+    const countT = {}
+    for(let i=0; i<s.length; i++){
+      countS[s[i]] = (countS[s[i]] || 0) + 1;
+      countT[t[i]] = (countT[t[i]] || 0) + 1;
+    }
+
+    for(const key in countS) {
+      if(countS[key] != countT[key])
+        return false;
+    }
+
+    return true;
+  }
+}
+
