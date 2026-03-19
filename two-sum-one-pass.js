cb659fa3b4dd7eb8e16e0f2a3f18be2e85ae02bb
renesanger <renejsanger@gmail.com>
2026-03-19 13:08:04 -0400

    Implement one-pass solution for two-sum problem
    Fix typo in variable name 'taget' to 'target'.

--- two-sum-one-pass.js
+++ two-sum-one-pass.js
@@ -1,1 +1,13 @@
-
+class Solution {
+  twoSum(nums, target) {
+    const prevMap = new Map();
+    for(let i=0; i<nums.length; i++) {
+      const diff = taget - nums[i];
+      if(prevMap.has(diff)) {
+        return [prevMap.get(diff), i];
+      }
+      prevMap.set(nums[i], i);
+    }
+    return [];
+  }
+}
