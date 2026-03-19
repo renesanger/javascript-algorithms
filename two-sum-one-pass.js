51642b2a76f6bb7874b95d1984c188acf16765b9
renesanger <renejsanger@gmail.com>
2026-03-19 13:08:55 -0400

    Fix typo in variable name 'taget' to 'target'

--- two-sum-one-pass.js
+++ two-sum-one-pass.js
@@ -2,7 +2,7 @@ class Solution {
   twoSum(nums, target) {
     const prevMap = new Map();
     for(let i=0; i<nums.length; i++) {
-      const diff = taget - nums[i];
+      const diff = target - nums[i];
       if(prevMap.has(diff)) {
         return [prevMap.get(diff), i];
       }
